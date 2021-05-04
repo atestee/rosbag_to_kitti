6d92daf05866a7b3cdd99887e277d807351a951e
atestee <ester@atlasova.cz>
2021-05-04 21:55:44 +0200

    save_artifact_data2 added to solve transforms between artifacts and the laser sensor

--- data_handler.py
+++ data_handler.py
@@ -17,3 +17,23 @@ def save_artifact_data(name, tf_buffer):
         file.write('\n')
     file.close()
 
+def save_artifact_data2(name, tf_buffer, parent_frame, stamps):
+    transforms = []
+    s = len(stamps)
+
+    # Subt artifacts: https://www.subtchallenge.com/resources/SubT_Cave_Artifacts_Specification.pdf
+    artifacts = ['backpack', 'phone', 'rescue_randy', 'rope', 'helmet', 'extinguisher', 'drill', 'vent']
+
+    for artifact in artifacts:
+        for i in range(1, 5):
+            if tf_buffer._frameExists(artifact + '_' + str(i)):
+                for j in range(s):
+                    t = rospy.Time(secs=stamps[j].secs, nsecs=stamps[j].nsecs)
+                    transforms.append(tf_buffer.lookup_transform_core(artifact + '_' + str(i), parent_frame, t))
+
+    file = open(name, 'w')
+    for tf in transforms:
+        for line in str(tf).split('\n'):
+            file.write(line + '\n')
+        file.write('\n')
+    file.close()
